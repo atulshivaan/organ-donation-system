af891e604ad4e861b2513e2668d531bbafc06570
Ankit raj <106767352+Shadowankit@users.noreply.github.com>
2023-08-11 22:58:49 +0530

    Add files via upload

---
+++ organ donation website/homepage.js
@@ -0,0 +1,8 @@
+
+  const loginBtn = document.getElementById("login-btn");
+  const loginForm = document.getElementById("login-form");
+
+  loginBtn.addEventListener("click", () => {
+    loginForm.classList.remove("hidden");
+  });
+
